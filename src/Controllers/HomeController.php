0af40083a26ec4a974a2f31c238ba10ce746a1cd
kinfemichaeldesse <kinfemichael.desse@real-digital.de>
2024-02-16 15:24:22 +0100

    pt-1 php-docker-mysql-nginx

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,10 +3,6 @@
 namespace App\Controllers;
 
 use App\Services\View;
-use App\Core\Request;
-use Doctrine\ORM\EntityManager;
-use App\Models\User;
-use App\Core\App;
 
 class HomeController extends BaseController {
 
@@ -24,88 +20,5 @@ class HomeController extends BaseController {
         return View::make('register', [
             'title'=> 'Signup page'
         ]);
-    }
-
-    public function authenticate(EntityManager $entityManager, Request $request) {
-        $user = $entityManager->getRepository(User::class)->findOneBy(['username'=> $request->get('username')]);
-        if (!$user) { 
-            return View::make(path: 'login', args: [
-                'title' => 'Signup page',
-                'errors' => [
-                    'username' => $request->get('username'),
-                    'body' => 'wrong username'
-                ],
-                'request' => App::resolve(Request::class)->getAll(),
-            ]);
-        }
-
-        if (!$this->checkPassword($user, $request->get('password'))) {
-            return View::make(path: 'login', args: [
-                'title' => 'Signup page',
-                'errors' => [
-                    'password' => $request->get('password'),
-                    'body' => 'wrong password'
-                ],
-                'request' => App::resolve(Request::class)->getAll(),
-            ]);
-        }
-
-        setActiveUser($user);
-        
-        $this->redirect(to: '/');
-    }
-
-    private function checkPassword(User $user, string $password) {
-        return $user->getPassword() === $password;
-    }
-
-    private function validateUser(User $user, EntityManager $entityManager) {
-        $existing = $entityManager->getRepository(User::class)
-        ->findOneBy(['username' => $user->getUsername()]);
-        
-        if(!empty($existing)) { 
-            throw new \Exception('Username is taken. please choose a different one');
-        }
-
-        $existing = $entityManager->getRepository(User::class)
-        ->findOneBy(['email' => $user->getEmail()]);
-
-        if(!empty($existing)) { 
-            throw new \Exception('This email address is registered with us.');
-        }
-    }
-
-    public function store(EntityManager $entityManager, Request $request) {
-        $name = $request->get('name');
-        $email = $request->get('email');
-        $password = $request->get('password');
-        $username = $request->get('username');
-        $user = new User();
-
-
-        try {
-            $user->setName($name);
-            $user->setUsername($username);
-            $user->setEmail($email);
-            $user->setPassword($password);
-
-            $this->validateUser(user: $user, entityManager: $entityManager);
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            $message= 'You have been successfully registered!!!';
-
-            setFlashMessage(message: $message);
-            $this->redirect(to: '/login');
-
-        } catch(\Exception $e) {
-            $message= $e->getMessage();
-            setFlashMessage(message: $message, type:'error');
-            return View::make(path: 'register', args: [
-                'title' => 'Signup page',
-                'request' => App::resolve(Request::class)->getAll(),
-            ]);
-        }
     }
 }
--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\LoginController;
 use App\Controllers\NoteController;
 use App\Services\Router;
 use App\Services\View;
@@ -9,16 +10,14 @@ $router = new Router();
 
 $router->get("/", [HomeController::class, "index"]);
 $router->get("/login", [HomeController::class, "login"])->only('guest');
-$router->post("/login", [HomeController::class,"authenticate"]);
 $router->get("/register", [HomeController::class, "register"])->only("index");
-$router->post("/register", [HomeController::class, "store"]);
+$router->post("/login", [LoginController::class,"authenticate"]);
+$router->post("/register", [LoginController::class, "store"]);
 $router->get("/notes", [NoteController::class,"index"])->only('auth');
 $router->post("/create-note", [NoteController::class, "create"])->only('auth');
 $router->get("/note", [NoteController::class,"get"])->only('auth');
 $router->patch("/note", [NoteController::class,"update"])->only('auth');
 $router->delete("/note", [NoteController::class,"destroy"])->only('auth');
-
-
 
 //  EDIT LATER and HANDLE IN CONTROLLER CLASS
 $router->get("/about", function() {
